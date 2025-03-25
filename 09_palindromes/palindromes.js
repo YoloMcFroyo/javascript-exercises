74a259bc55283a577af3c95e64a3da2dffc12650
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 15:07:11 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let l = 0;
+  let r = word.length;
+  while(l < r){
+    while(!/[a-zA-Z0-9]/.test(word.charAt(l)) && l < r){
+      l++;
+    }
+    while(!/[a-zA-Z0-9]/.test(word.charAt(r)) && l < r){
+      r--;
+    }
+    if(word.charAt(l).toLowerCase() !== word.charAt(r).toLowerCase()){
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
 };
 
 // Do not edit below this line

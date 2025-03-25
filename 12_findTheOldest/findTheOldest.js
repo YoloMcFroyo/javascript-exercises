7405077ce827a96dca1c198449b9fac80ebcf7d9
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 15:31:23 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let oldestAge = 0;
+  let age;
 
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].yearOfBirth !== undefined && people[i].yearOfDeath === undefined) {
+      age = new Date().getFullYear() - people[i].yearOfBirth;
+    } else {
+      age = people[i].yearOfDeath - people[i].yearOfBirth;
+    }
+    if (age > oldestAge) {
+      oldestPerson = people[i];
+      oldestAge = age;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line

76dd527d3ef8dfe323d7c63d0f41a22f06639316
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 14:41:15 -0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let rollingSum = 0;
+	for(let i = 0; i < nums.length; i++){
+    rollingSum += nums[i];
+  }
+  return rollingSum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  rollingSum = 1;
+  for(let i = 0; i < nums.length; i++){
+    rollingSum *= nums[i];
+  }
+  return rollingSum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let rollingPower = 1;
+	for(let i = 0; i < num2; i++){
+    rollingPower *= num1;
+  }
+  return rollingPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {  
+  let rollingSum = 1;
+  for(let i = num1; i > 0; i--){
+    rollingSum *= i;
+  }
+  return rollingSum;
 };
 
 // Do not edit below this line

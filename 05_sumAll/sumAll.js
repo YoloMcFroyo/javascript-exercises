b411896bfbc7e98bdd215b61886147df038ae033
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 13:57:00 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...integers) {
+  if(integers.length > 2){
+    return 'ERROR';
+  }
+  if(!Number.isInteger(integers[0]) || !Number.isInteger(integers[1])){
+    return 'ERROR';
+  }
+  if(integers[0] < 0 || integers[1] < 0){
+    return 'ERROR';
+  }
 
+  let end = Math.max(integers[0], integers[1]);
+  let start = Math.min(integers[0], integers[1]);
+  let runningTotal = 0;
+
+  for(let curNumber = start; curNumber <= end; curNumber++){
+    runningTotal += curNumber;
+  }
+  return runningTotal;
 };
 
 // Do not edit below this line

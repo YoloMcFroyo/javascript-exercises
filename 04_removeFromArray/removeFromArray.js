cec82d9691a228106de8a9831541cf57e9edad08
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 13:33:53 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...excluded) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if (!excluded.includes(array[i])){
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line

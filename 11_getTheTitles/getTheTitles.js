97a491ea93168f0662d7c7f2b1258f75ad9ddc79
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-25 15:16:48 -0700

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfBooks) {
+  let titles = [];
+  for(let i = 0; i < arrayOfBooks.length; i++){
+    titles.push(arrayOfBooks[i].title);
+  }
+  return titles;
 };
 
 // Do not edit below this line

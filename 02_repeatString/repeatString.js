2e635f391460a40011898a786ef90f5444352d76
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-03-24 18:36:53 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function( word, numRepeated) {
+  if(numRepeated < 0) return 'ERROR'
+  let builtString = '';
+  for(let i = 0; i < numRepeated; i++){
+    builtString += word;
+  }
+  return builtString;
 };
 
 // Do not edit below this line
